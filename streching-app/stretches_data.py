bbc3cf2133c9d8b32e84e1b8067018f7d2484c10
KSY726 0 <syku0903@gmail.com>
2025-05-26 06:03:08 +0000

    1

---
+++ streching-app/stretches_data.py
@@ -0,0 +1,14 @@
+STRETCHES = {
+    "lying": [
+        {"name": "무릎 당기기 스트레칭", "url": "https://www.youtube.com/watch?v=abcd123"},
+        {"name": "허리 비틀기 스트레칭", "url": "https://www.youtube.com/watch?v=efgh456"}
+    ],
+    "sitting": [
+        {"name": "목 돌리기", "url": "https://www.youtube.com/watch?v=ijkl789"},
+        {"name": "어깨 올리기", "url": "https://www.youtube.com/watch?v=mnop012"}
+    ],
+    "standing": [
+        {"name": "종아리 스트레칭", "url": "https://www.youtube.com/watch?v=qrst345"},
+        {"name": "전신 스트레칭", "url": "https://www.youtube.com/watch?v=uvwx678"}
+    ]
+}
